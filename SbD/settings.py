4026d8f36c437f87a3a667f42e8a7296953f457c
Henn1ngS <hendrik.spiess14@gmail.com>
2022-12-04 22:22:36 +0100

    postgress

--- SbD/settings.py
+++ SbD/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Gesundheitsakte',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,16 @@ WSGI_APPLICATION = 'SbD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'postgres',
+                'USER': 'postgres',
+                'PASSWORD': 'Genshin',
+                'HOST': 'localhost',
+                'PORT': '5433',
+        },
     }
-}
+
+
 
 
 # Password validation
